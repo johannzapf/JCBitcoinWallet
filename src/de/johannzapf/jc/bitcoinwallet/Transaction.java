4c2daf0d94d7c518eebe336c5050d2a83e778d0d
Johann Theo Zapf <jozapf@mail.uni-mannheim.de>
2021-10-29 14:46:35 +0200

    fix memory bug (make sha and signature transient)

--- src/de/johannzapf/jc/bitcoinwallet/Transaction.java
+++ src/de/johannzapf/jc/bitcoinwallet/Transaction.java
@@ -10,9 +10,6 @@ import javacardx.crypto.Cipher;
 
 public class Transaction {
 
-    private byte[] sha;
-    private byte[] signature;
-
     private byte[] targetAddressPKH; // Pubkeyhash of target address
     private byte[] amount; // amount to spent (in satoshis)
     private byte[] change; // amount that goes back to address (in satoshis)
@@ -26,8 +23,6 @@ public class Transaction {
 
 
     public Transaction(){
-        sha = new byte[32];
-        signature = new byte[72];
         targetAddressPKH = new byte[20];
         amount = new byte[8];
         change = new byte[8];
@@ -83,6 +78,7 @@ public class Transaction {
             byte[] toSign = getDoubleHashedTx(pubKeyHash, i); //Retrieve SHA-256 Hash that needs to be signed
 
             //The actual Signature
+            byte[] signature = JCSystem.makeTransientByteArray((short) 72, JCSystem.CLEAR_ON_DESELECT);
             short sigLength = sign.sign(toSign, (short) 0, (short) 32, signature, (short) 0);
             sigLength += CryptoUtils.fixS(signature, (short) 0);
 
@@ -160,6 +156,7 @@ public class Transaction {
         toSign[(short)(offset + 73)] = 0x01; //Sig Hash Code
 
         //Double SHA-256 entire structure
+        byte[] sha = JCSystem.makeTransientByteArray((short) 32, JCSystem.CLEAR_ON_DESELECT);
         sha256.doFinal(toSign, (short) 0, (short) toSign.length, sha, (short) 0);
         sha256.doFinal(sha, (short) 0, (short) sha.length, sha, (short) 0);
 
